497bf87aa75e0adfaa2f97685926983d15887f49
TashvithRathod <rathodtashvithnayak@gmail.com>
2026-08-13 16:40:19 +0530

    i hae created a card and toggle buttons

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,26 @@
-alert("welcome to bootcam babyyyy");
+var users = [
+    {
+        "name": "john doe",
+        "gender": "male",
+        "image": "john.png"
+    },
+    {
+        "name": "jane doe",
+        "gender": "female",
+        "image": "jane.png"
+    }
+];
+
+var curId = 0;
+
+function toggleUser() {
+    curId = (curId + 1) % 2;
+
+    var userName = document.getElementById("user-name");
+    var userImage = document.getElementById("user-image");
+    var userGender = document.getElementById("user-gender");
+
+    userName.innerHTML = users[curId].name;
+    userGender.innerHTML = users[curId].gender;
+    userImage.src = users[curId].image;
+}
